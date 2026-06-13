90b97e57117600e350195d90d1aa9e8a87ab725b
byq1337 <qbitdev1337@gmail.com>
2026-06-13 15:42:53 +0300

    Delete qrevise directory

--- qrevise/dev/qbit/qrevise/util/Colorize.java
+++
@@ -1,9 +0,0 @@
-﻿package dev.qbit.qrevise.util;
-
-import org.bukkit.ChatColor;
-
-public class Colorize {
-   public static String s(String s) {
-      return ChatColor.translateAlternateColorCodes('&', s);
-   }
-}
--- qrevise/dev/qbit/qrevise/util/Timer.java
+++
@@ -1,9 +0,0 @@
-﻿package dev.qbit.qrevise.util;
-
-public class Timer {
-   public static String getTimer(int seconds) {
-      int minutes = seconds / 60;
-      int remainingSeconds = seconds % 60;
-      return minutes == 0 ? remainingSeconds + " сек" : minutes + " мин " + remainingSeconds + " сек";
-   }
-}
